7ac67e0ee56e1fa1e0251f1c28f7db9088545f00
tsp <milo@milogert.com>
2013-09-25 14:06:33 -0500

    added some stuff to main.py along with a crappy calendar in index. additionally added bootstrap.

--- main.py
+++ main.py
@@ -3,15 +3,16 @@
 import web
 
 import config
-import form
 import model
 from view import render as render
+from datetime import date
 
 class Index:
 
     def GET(self):
         """Show index."""
-        return render.index()
+        d = date
+        return render.index(d)
 
 config.app.internalerror = web.debugerror
 #application = config.app.wsgifunc()
