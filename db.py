81734482d817502c56c23bc77a9ce7d1bb7164c8
tsp <milo@milogert.com>
2013-09-25 13:41:38 -0500

    changed db host.

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ import web
 
 # Normal database for operations on the data this pertains to.
 data = web.database(
-#    host='192.168.0.100',
-#    port=3306,
+    host='milogert.com',
+    port=3306,
     dbn='mysql',
     db='tsp',
     user='tsp',
